2ebe31a4a3872aebf002b37e484adfd4136cb827
Tri Anh <anhmht@gmail.com>
2023-05-30 02:30:25 +0700

    test fb sdk

--- static/js/fb-sdk.js
+++ static/js/fb-sdk.js
@@ -9,11 +9,9 @@ window.fbAsyncInit = function () {
 ;(function (d, s, id) {
   var js,
     fjs = d.getElementsByTagName(s)[0]
-  if (d.getElementById(id)) {
-    return
-  }
+  if (d.getElementById(id)) return
   js = d.createElement(s)
   js.id = id
-  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js'
+  js.src = 'https://connect.facebook.net/vn_VN/sdk/xfbml.customerchat.js'
   fjs.parentNode.insertBefore(js, fjs)
 })(document, 'script', 'facebook-jssdk')
